94c97bccfe0855b235ee14a7813739f14e148de2
Marlon Guzman <marlonguzman2997@gmail.com>
2022-08-16 18:14:09 -0500

    4

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,14 @@
-import React, {useRef} from 'react'
+import React, { useRef } from 'react'
 import Navbar from '../Navbar/Navbar'
 import style from "../Home/Home.module.css"
 import logoGithub from "../../Image/5847f98fcef1014c0b5e48c0.png"
 import logoIn from "../../Image/icone-linkedin-ronde-noire.png"
 import logoWP from "../../Image/what.png"
-import Pi from "../../Image/Pi-Pokemons.png"
+import Pi from "../../Image/Pokemons.png"
 import { frontEnd, backEnd, otras } from '../Habilities'
 import CardHability from '../Card/CardHability'
 import Footer from '../Footer/Footer'
+import beerland from "../../Image/Beerland.png"
 
 export default function Home() {
     const scroll0 = useRef()
@@ -18,66 +19,62 @@ export default function Home() {
     const scroll5 = useRef()
     return (
         <div>
-            <Navbar scroll0={scroll0} scroll1={scroll1} scroll2={scroll2} scroll3={scroll3} scroll4={scroll4} scroll5={scroll5}  />
-            {/* <div className={style.NameContainer}>
-
-                <div>
-                    <span className={style.Text}>Soy </span>
-                </div>
-                <div>
-                    <span className={style.Text}>Marlon Guzman </span>
-                </div>
-                <div>
-                    <span className={style.Text}>Full Stack</span>
-                </div>
-                <div>
-                    <span className={style.Text}>Web Developer</span>
-                </div> */}
-<div className={style.head}>
-<div ref={scroll0} className={style.NameContainer}>
-          <span className={style.commonText}>SOY</span>
-        <div className={style.blueBackground} style={{marginBottom:"8px"}}>
-
-          <span className={style.highText}>MARLON GUZMAN</span>
-
-        </div>
-        <div className={style.blueBackground}>
-
-          <span className={style.highText}>FULL STACK</span>
-
-        </div>
-          <span className={style.commonText}>WEB DEVELOPER</span>
-                <div className={style.Social}>
-                    <a href="https://github.com/mguz29" target="_blank" rel="noreferrer" className={style.image}><img src={logoGithub} /></a>
-                    <a href="https://www.linkedin.com/in/mguz29" target="_blank" rel="noreferrer" className={style.image}><img src={logoIn} /></a>
-                    <a href="https://wa.me/573223313796" target="_blank" rel="noreferrer" className={style.image}><img src={logoWP} /></a>
-                </div>
+            <div>
+            <Navbar scroll0={scroll0} scroll1={scroll1} scroll2={scroll2} scroll3={scroll3} scroll4={scroll4} scroll5={scroll5} />
             </div>
+            <div className={style.head}>
+                <div ref={scroll0} className={style.NameContainer}>
+                    <span className={style.commonText}>SOY</span>
+                    <div className={style.blueBackground} style={{ marginBottom: "8px" }}>
+
+                        <span className={style.highText}>MARLON GUZMAN</span>
+
+                    </div>
+                    <div className={style.blueBackground}>
+
+                        <span className={style.highText}>FULL STACK</span>
+
+                    </div>
+                    <span className={style.commonText}>WEB DEVELOPER</span>
+                    <div className={style.Social}>
+                        <a href="https://github.com/mguz29" target="_blank" rel="noreferrer" className={style.image}><img src={logoGithub} /></a>
+                        <a href="https://www.linkedin.com/in/mguz29" target="_blank" rel="noreferrer" className={style.image}><img src={logoIn} /></a>
+                        <a href="https://wa.me/573223313796" target="_blank" rel="noreferrer" className={style.image}><img src={logoWP} /></a>
+                    </div>
+                </div>
             </div>
 
 
             <section ref={scroll1} id="about" className={style.aboutPageContainer}>
 
- 
-           <div  className={style.blueBackground} style={{margin:"4rem"}}>
-      <span  className={style.highText}>Sobre Mi</span>
-      </div>
+
+                <div className={style.blueBackground} style={{ margin: "4rem" }}>
+                    <span className={style.highText}>Sobre Mi</span>
+                </div>
                 <div className={style.descriptionContainer}>
                     <div className={style.description}>
                         <span className={style.textDescription}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <br /><br />
+                            Desarrollador Fullstack JavaScript, apasionado por el Front-end.
+                            Realicé un bootcamp de más de 800 hs. de estudio intensivo que me preparó para desempeñarme como desarrollador Fullstack. 
+                            Dentro del curso aprendí desde lo básico hasta desarrollar una aplicación en equipo.
+
+                            Me inicie en el mundo del desarrollo hace relativamente poco. 
+                            Quede fascinado con lo que podía crear solo escribiendo. 
+                            Hoy busco oportunidades que ayuden a mi desarrollo personal. 
+                            Soy una persona proactiva, con buena disposición, 
+                            siempre dispuesta, respetuosa y por sobre todo con ganas de trabajar en equipo.
+                            {/* Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <br /><br />
                         when an unknown printer took a galley of type and scrambled it to make
                             {/* <strong><a href="https://certificates.soyhenry.com/cert?id=9a14a737-bb3c-47ca-ac61-7924de3cbdf7"
                                 target="_blank"
                                 rel="noreferrer"
                                 className={style.Link}> (ver certificado)</a></strong> */}
-                                 donde realicé varios de los proyectos
+                            {/* donde realicé varios de los proyectos
                                  a type specimen book. It has survived not only five centuries, but also the leap into
                                  electronic typesetting, remaining essentially unchanged. It was popularised
                             y guiarlos en su proceso de aprendizaje. <br /><br />
                             Actualmente estoy buscando trabajar en un lugar que me permita dejar mi marca en la web y con el que
-                            pueda crecer profesionalmente.
+                            pueda crecer profesionalmente. */}
                         </span>
                         <div className={style.TwhiteBackground} style={{ marginTop: "45px", marginBottom: "40px", marginLeft: "-4rem", cursor: "pointer" }}>
                             <a href="https://drive.google.com/file/d/1vkahmbxmi4ly8kmXL7AxEJ3sQNWZNJzD/view?usp=sharing" target="_blank" rel="noreferrer" className={style.Link} ><span className={style.subTitle} style={{ color: "#fff" }}>DESCARGAR CV</span></a>
@@ -89,9 +86,9 @@ export default function Home() {
 
             <div className={style.ContainerHabilities}>
 
-            <div className={style.blueBackground} style={{margin:"4rem"}}>
-      <span ref={scroll2} className={style.highText}>Habilidades</span>
-      </div>
+                <div className={style.blueBackground} style={{ margin: "4rem" }}>
+                    <span ref={scroll2} className={style.highText}>Habilidades</span>
+                </div>
 
                 <div className={style.Habilities}>
                     <div>
@@ -180,9 +177,9 @@ export default function Home() {
          </div>
         </div> */}
 
-<div className={style.blueBackground} style={{margin:"4rem"}}>
-      <span ref={scroll3} className={style.highText}>PROYECTOS</span>
-      </div>
+            <div className={style.blueBackground} style={{ margin: "4rem" }}>
+                <span ref={scroll3} className={style.highText}>PROYECTOS</span>
+            </div>
             <div className={style.projectsContainer}>
 
                 <div className={style.projectContainer}>
@@ -190,10 +187,12 @@ export default function Home() {
                         <div className={style.projectDesc}>
                             <label className={style.pjtitle} style={{ fontWeight: "800", fontSize: "35px", marginBottom: "10px", cursor: "text" }}>Ecommerce Beerland</label>
                             <label style={{ fontSize: "19px", cursor: "text" }}>
-                            industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                            unchanged. It was popularised in the 1960s with the release of Letraset heets containing Lorem Ipsum passages,
-                            nd more recently with desktop publishing software like Aldus PageMaker including versions of Lorem</label>
+                                Beerland es un E-commerce realizado para la etapa final de Henry,podemos ver un dashboard
+                                para un usuario(consumidor) con funcionalidades de autenticacion, favoritos, comentarios y compras
+                                Tambien contamos con el dashboard de un admin (vendedor) podra ver las estadisticas como 
+                                sus ventas,ganancias, stock disponible
+                                Contamos con un SuperAdmin se encarga de todas las funciones como
+                                venta de productos, pago a los vendedores y solucion de problemas  </label>
                         </div>
 
                         <div className={style.buttons}>
@@ -202,7 +201,7 @@ export default function Home() {
                                     <span className={style.projectsubTitle} style={{ color: "#FE007C" }}>DEMO</span>
                                 </a>
                             </div>
-                            <a  className={style.Link} href="https://github.com/lezcanozarza/TechStoreEcommerce" target="_blank" rel="noreferrer">
+                            <a className={style.Link} href="https://github.com/lezcanozarza/TechStoreEcommerce" target="_blank" rel="noreferrer">
                                 <div className={style.projectwhiteBackground}>
                                     <span className={style.projectsubTitle} style={{ color: "#FE007C" }}>REPOSITORIO</span>
                                 </div>
@@ -210,7 +209,7 @@ export default function Home() {
                         </div>
                     </div>
                     <div className={style.projectImg}>
-                        <img style={{ width: "400px" }} src={'https://www.cinemascomics.com/wp-content/uploads/2020/08/goku-dragon-ball-super-ultra-instinto.jpg'} alt="" className={style.imagen} />
+                        <img style={{ width: "600px" }} src={beerland} alt="" className={style.imagen} />
                     </div>
                 </div>
             </div>
@@ -224,10 +223,10 @@ export default function Home() {
                         <div className={style.projectDesc}>
                             <label className={style.pjtitle} style={{ fontWeight: "800", fontSize: "35px", marginBottom: "10px", cursor: "text" }}>SPA Pokemon</label>
                             <label style={{ fontSize: "19px", cursor: "text" }}>
-                            industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                            unchanged. It was popularised in the 1960s with the release of Letraset heets containing Lorem Ipsum passages,
-                            nd more recently with desktop publishing software like Aldus PageMaker including versions of Lorem</label>
+                            Presento el PI que desarrollé en el bootcamp de Henry, creamos una API (backend) a partir del consumo de otra API de Pokémon,
+                            generando consulta de la DataBase propia, y mediante la librearía React , presentando una serie de cartas, describiendo al azar 40 Pokémon de toda la base de datos disponible,
+                             en donde podemos interactuar con esta, de forma entretenida: filtrando, organizando y creando nuevos Pokémon.
+                            </label>
                         </div>
 
                         <div className={style.buttons}>
@@ -244,14 +243,14 @@ export default function Home() {
                         </div>
                     </div>
                     <div className={style.projectImg}>
-                    <a className={style.Link} href="https://pi-pokemons-mguz.vercel.app/" target="_blank" rel="noreferrer">
-                        <img style={{ width: "600px" }} src={Pi} alt="" className={style.imagen} />
-                    </a>
+                        <a className={style.Link} href="https://pi-pokemons-mguz.vercel.app/" target="_blank" rel="noreferrer">
+                            <img style={{ width: "600px" }} src={Pi} alt="" className={style.imagen} />
+                        </a>
                     </div>
                 </div>
             </div>
 
-            <Footer scroll0={scroll0} scroll1={scroll1} scroll2={scroll2} scroll3={scroll3}/>
+            <Footer scroll0={scroll0} scroll1={scroll1} scroll2={scroll2} scroll3={scroll3} />
         </div>
     )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,31 +6,32 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
-export default function Navbar({scroll0, scroll1, scroll2, scroll3}) {
-  
+export default function Navbar({ scroll0, scroll1, scroll2, scroll3 }) {
+
   var [render, setRender] = useState({
     Marlon: false,
     Home: false,
     About: false,
     Projects: false,
-    Habilities:false
-})
+    Habilities: false
+  })
 
-const handleClick = (e, bool) => {
+  const handleClick = (e, bool) => {
     e.preventDefault();
     if (bool) setRender({ ...render, [e.target.id]: true })
     else setRender({ ...render, [e.target.id]: !render[e.target.id] })
-}
+  }
   return (
-    <div>
-        <div className={style.Navbar}>
-          <Link to='/Home' id='Marlon' onClick={(e) => { scroll0.current.scrollIntoView({ behavior: "smooth" }); handleClick(e, true) }} className={style.Link}> <h5>Marlon Guzman</h5>  </Link>
-          <Link to='/Home' id='Home' onClick={(e) => { scroll0.current.scrollIntoView({ behavior: "smooth" }); handleClick(e, true) }} className={style.Link}> <h5>Home</h5>  </Link>
-          <Link to='/Home' id='About' onClick={(e) => { scroll1.current.scrollIntoView({ behavior: "smooth" }); handleClick(e, true) }} className={style.Link}> <h5>About Me</h5>  </Link>
-          <Link to='/Home' id='Projects' onClick={(e) => { scroll3.current.scrollIntoView({ behavior: "smooth" }); handleClick(e, true) }} className={style.Link}> <h5>Projects</h5> </Link>
-          <Link to='/Home' id='Habilities' onClick={(e) => { scroll2.current.scrollIntoView({ behavior: "smooth" }); handleClick(e, true) }} className={style.Link}> <h5>Habilities</h5> </Link>
-        </div>
+    <div className={style.Container}>
+      <div className={style.Navbar}>
+        <Link to='/Home' id='Marlon' onClick={(e) => { scroll0.current.scrollIntoView({ behavior: "smooth" }); handleClick(e, true) }} className={style.Link}> <h5>Marlon Guzman</h5>  </Link>
+        <Link to='/Home' id='Home' onClick={(e) => { scroll0.current.scrollIntoView({ behavior: "smooth" }); handleClick(e, true) }} className={style.Link}> <h5>Home</h5>  </Link>
+        <Link to='/Home' id='About' onClick={(e) => { scroll1.current.scrollIntoView({ behavior: "smooth" }); handleClick(e, true) }} className={style.Link}> <h5>About Me</h5>  </Link>
+        <Link to='/Home' id='Projects' onClick={(e) => { scroll3.current.scrollIntoView({ behavior: "smooth" }); handleClick(e, true) }} className={style.Link}> <h5>Projects</h5> </Link>
+        <Link to='/Home' id='Habilities' onClick={(e) => { scroll2.current.scrollIntoView({ behavior: "smooth" }); handleClick(e, true) }} className={style.Link}> <h5>Habilities</h5> </Link>
+      </div>
     </div>
+
 
   )
 }
